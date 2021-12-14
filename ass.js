69b6ff9fac2ff6426b2d6ba3453aa2369744f1c5
Phani54321 <92142841+Phani54321@users.noreply.github.com>
2021-12-14 19:12:06 +0530

    m

---
+++ ass.js
@@ -0,0 +1,29 @@
+var num1=2;
+var num2=3;
+var num3=5;
+num1+= 9
+console.log(num1);
+
+var num1=2;
+var num2=3;
+var num3=5;
+num2-=8;
+console.log(num2);
+
+var num1=2;
+var num2=3;
+var num3=5;
+num3*=6;
+console.log(num3);
+
+var num1=2;
+var num2=3;
+var num3=5;
+num3/=7;
+console.log(num3);
+
+var num1=2;
+var num2=3;
+var num3=5;
+num2%=8;
+console.log(num2);
